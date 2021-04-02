0d6c75844db03f8bfd6d7c2a68850bf404208089
josephakeni <josephakeni@gmail.com>
2021-04-02 09:05:05 +0100

    added ntp module

--- manifests/init.pp
+++
@@ -1,7 +0,0 @@
-class apache {
-	include apache::package
-	include apache::config
-	include apache::service
-	
-	Class['apache::package'] -> Class['apache::config'] ~> ['apache::service']
-}
--- manifests/package.pp
+++
@@ -1,8 +0,0 @@
-class apache::package (
-	String $packagename=lookup({"name"=>"apache.packagename", "default_value" => "httpd"})
-	) {
-	package { 'apache_package':
-		name	=>  $packagename,
-		ensure  =>  present
-	}
-}
